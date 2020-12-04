d4438750b006dd2e4219d8a06abb3b707e868c95
Self_C <not_forgiving@list.ru>
2020-12-04 14:46:43 +0300

    add correct deleted not use vareble

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Footer from "./components/Footer/Footer";
 import Fullscreen from "./components/Fullscreen/Fullscreen";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
-import { Route, NavLink, BrowserRouter, Switch } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
